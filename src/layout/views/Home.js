b005cc6a381ff9b8e7b000ddc0225f49f38e5671
mjwills-inf <mjwills@live.co.uk>
2020-12-13 15:53:40 +0000

    .

--- src/layout/views/Home.js
+++ src/layout/views/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './Home.css'
 
 import Booklist from '../../components/Booklist'
+import logo from '../../assets/logo.svg'
 
 export default function Home() {
   return (
@@ -15,21 +16,17 @@ export default function Home() {
       </div>
 
       <div id="page-titles">
-      <img
-        id="vue-logo"
-        alt="Vue logo"
-        src=""
-      />
-      <h1>Library. <span>Dark.</span></h1>
-      <h3>The Odin Project</h3>
-      <h3>Front-End Framework Project: Vue Library</h3>
-    </div>
-    <div id="page-functions">
-      <button>Add New Book</button>
-    </div>
-    <div id="page-cards">
-      
-    </div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Library. <span>Dark.</span></h1>
+        <h3>The Odin Project</h3>
+        <h3>Front-End Framework Project: React Library</h3>
+      </div>
+      <div id="page-functions">
+        <button>Add New Book</button>
+      </div>
+      <div id="page-cards">
+        
+      </div>
     </div>
   )
 }
