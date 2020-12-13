b35cf5f3e14641e41a13607742853386b9c539a4
mjwills-inf <mjwills@live.co.uk>
2020-12-13 13:54:03 +0000

    .

---
+++ src/components/Booklist.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Booklist.css'
+
+export default function Booklist() {
+  return (
+    <div>
+      Booklist
+    </div>
+  )
+}
--- src/layout/views/Home.js
+++ src/layout/views/Home.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './Home.css'
+
+import Booklist from '../../components/Booklist'
 
 export default function Home() {
   return (
-    <div id="card-container">
-      Home
+    <div id="home">
+      <div className="modal-container">
+      
+      </div>
+
+      <div className="modal-container">
+      
+      </div>
+
+      <div id="page-titles">
+      <img
+        id="vue-logo"
+        alt="Vue logo"
+        src=""
+      />
+      <h1>Library. <span>Dark.</span></h1>
+      <h3>The Odin Project</h3>
+      <h3>Front-End Framework Project: Vue Library</h3>
+    </div>
+    <div id="page-functions">
+      <button>Add New Book</button>
+    </div>
+    <div id="page-cards">
+      
+    </div>
     </div>
   )
 }
